6e8bfd5a9182c117fb272517aa1bf6420cd3a14b
philipappiah <pappiahkubi1@gmail.com>
2019-06-10 13:27:09 +0100

    third commit

--- controllers/myapp.js
+++ controllers/myapp.js
@@ -1,27 +1,27 @@
 
 
 exports.getdo = (req, res, next) =>{
-    res.render('index');
+    res.render('index', {index:true, about:false, research:false, admissions:false, news:false,  academics:false});
 }
 
 exports.about = (req, res, next) =>{
-    res.render('about');
+    res.render('about',{index:false, about:true, research:false, admissions:false, news:false, academics:false});
 }
 exports.academics = (req, res, next) =>{
-    res.render('academics');
+    res.render('academics',{index:false, about:false, research:false, admissions:false, news:false, academics:true});
 }
 
 
 exports.research = (req, res, next) =>{
-    res.render('research');
+    res.render('research',{index:false, about:false, research:true, admissions:false, news:false,  academics:false});
 }
 
 exports.admissions = (req, res, next) => {
-    res.render('admissions');
+    res.render('admissions',{index:false, about:false, research:false, admissions:true, news:false,  academics:false});
 }
 
 exports.news = (req, res, next) =>{
-    res.render('news');
+    res.render('news',{index:false, about:false, research:false, admissions:false, news:true,  academics:false});
 }
     
 exports.postdata = (req, res, next) =>{
